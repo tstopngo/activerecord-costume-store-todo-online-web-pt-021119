26fa93cd38d2c4133432e579dd5231cf4cce9162
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 02:23:16 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.string "location"
     t.integer "costume_inventory"
-    t.integer "employee_count"
+    t.integer "num_of_employees"
     t.boolean "in_business"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
