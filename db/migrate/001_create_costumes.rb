4dc90ea5632d9ca2a7bd43f7e01414737e316995
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 02:05:07 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,13 +10,12 @@
 
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
-    create_table do |t|
+    create_table :costimes do |t|
       t.string :name
       t.integer :price
       t.string :size
       t.string :image_url
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.timestamps
     end
   end
 end
