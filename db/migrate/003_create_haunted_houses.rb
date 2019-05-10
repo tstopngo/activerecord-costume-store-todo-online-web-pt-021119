1ab6d8a4ab80fae24845c7dd2e70693fe968cd95
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 02:14:21 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,10 +4,12 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_houses do |t|
       t.string :name
+      t.string :location
+      t.string :theme
       t.integer :price
-      t.string :size
-      t.string :image_url
+      t.boolean :family_friendly
       t.timestamps
+      t.string :long_description
     end
   end
 end
