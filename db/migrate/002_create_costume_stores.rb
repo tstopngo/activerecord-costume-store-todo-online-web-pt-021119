5db7cb7ac662808f0c1d008bcef79a7457da380b
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 02:23:54 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.boolean :in_business
+      t.boolean :still_in_business
       t.timestamps
     end
   end
