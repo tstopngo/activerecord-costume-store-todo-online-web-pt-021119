3d6328980705b5c0c125ecdd281223c517e125a4
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 02:27:24 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,8 +8,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.datetime :opening_time
-      t.datetime :closing_time
+      t.float :opening_time
+      t.float :closing_time
     end
   end
 end
