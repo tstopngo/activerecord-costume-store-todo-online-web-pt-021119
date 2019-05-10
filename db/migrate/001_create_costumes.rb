1339d2e63a83476551effc2d52af1af8890d60e3
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 01:57:08 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,6 +15,8 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.integer :price
       t.string :size
       t.string :image_url
+      t.datetime :created_at
+      t.datetime :updated_at
     end
   end
 end
