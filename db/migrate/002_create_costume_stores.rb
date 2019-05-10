ba6c223aefcbc4a419312710c640393de8a7c835
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 02:17:29 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,6 +1,6 @@
 # Create your costume_stores migration here
 
-class CreateCostumeStores < ActiveRecord::Migration[5.1]
+class CreateCostumeStores < ActiveRecord::Migration
   def change
     create_table :costume_stores do |t|
       t.string :name
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,6 @@
 # Create your haunted_houses migration here
 
-class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+class CreateHauntedHouses < ActiveRecord::Migration
   def change
     create_table :haunted_houses do |t|
       t.string :name
