82fa36f467a20764ab12a8ebb21847f7247ae22f
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 02:11:52 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+
+class CreateCostumes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costimes do |t|
+      t.string :name
+      t.integer :price
+      t.string :size
+      t.string :image_url
+      t.timestamps
+    end
+  end
+end
