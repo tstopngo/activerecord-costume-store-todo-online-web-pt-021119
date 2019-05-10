4df964b4b02dd4e8c56c44a8af510072f8817df3
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 02:30:02 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.integer :price
       t.boolean :family_friendly
       t.datetime :opening_date
-      t.float :closing_date
+      t.datetime :closing_date
       t.string :long_description
     end
   end
